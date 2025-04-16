516c8fb615a5676d521ed1dd950b1f6fa886ee3a
Avinash Boruah <avinashboruah8@gmail.com>
2025-04-16 23:28:21 +0530

    host the app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import 'dotenv/config'
 import connectDB from "./db/index.js";
-
+import { app } from './app.js';
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`your app is listening in ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.error("Database is not connected", error)
+})
