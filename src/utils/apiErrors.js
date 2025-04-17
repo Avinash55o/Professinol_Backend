42769050400bad92c5fd3c6434b5b9ecbf930a50
Avinash Boruah <avinashboruah8@gmail.com>
2025-04-17 22:24:51 +0530

    utils for error handeling

---
+++ src/utils/apiErrors.js
@@ -0,0 +1,21 @@
+class apiErrors extends Error(){
+    constructor(
+        statusCode,
+        message="something went wrong",
+        errors=[],
+        statck=""
+        ){
+            super(message)
+            this.statusCode= statusCode
+            this.errors= errors
+            this.data=null
+            this.message=message
+            this.success=false
+
+            if (statck){
+                this.stack=statck
+            }else{
+                Error.captureStackTrace(this, this.constructor)
+            }
+        }
+}
