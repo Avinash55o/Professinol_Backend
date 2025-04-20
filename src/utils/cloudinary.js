850f07563b4dbb810f916836da325c86dd85bc5a
Avinash Boruah <avinashboruah8@gmail.com>
2025-04-20 16:36:57 +0530

    added a util file for cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const UploadToCloudinary=async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        const response= await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+        //file upload successfull
+        console.log("file is uploaded to the cloudinary", response.url);
+        return response; 
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
