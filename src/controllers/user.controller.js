54749cc0a3cb31a7d54c878253f50f0b475b3029
Avinash Boruah <avinashboruah8@gmail.com>
2025-04-20 22:57:43 +0530

    add register controller and bug fixing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,63 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {apiErrors} from "../utils/apiErrors.js"
+import { User } from "../models/user.models.js";
+import { UploadToCloudinary } from "../utils/cloudinary.js";
+import { apiResponse } from "../utils/apiResponse.js";
 
 const registerUser= asyncHandler(async(req,res)=>{
-    res.status(200).json(
+    const {fullName, email, userName, password}= req.body;
+
+    if (
+        [fullName, email, userName, password].some((fields)=> fields?.trim()=== "")
+    ) {
+        throw new apiErrors(400, "all fields are required")
+    }
+   // We can do this so we can find if it exist or not .. u can also check multiple
+    // User.findOne({userName})
+    const existedUser=User.findOne(
         {
-            message: "all ok"
+            $or:[{userName},{email}]
         }
     )
-})
+
+    if (existedUser) {
+        throw new apiErrors(409, "userName or eamil already exist")
+    }
+
+    const avatarLocalPath=req.files?.avatar[0]?.path;
+    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new apiErrors(400, "avatar is required")
+    }
+
+    const avatar=await UploadToCloudinary(avatarLocalPath);
+    const coverImage= await UploadToCloudinary(coverImageLocalPath);
+
+    if (!avatar) {
+        throw new apiErrors(400, "avatar is required")
+    }
+
+    const user=await User.create({
+        fullName,
+        avatar:avatar.url,
+        coverImage:coverImage?.url || "",
+        userName,
+        password,
+        email
+    })
+
+   const createdUser= User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new apiErrors(500, "somthing went wrong while creating the user")
+    }
+
+    return res.status(2001).json(
+        new apiResponse(200, createdUser, "user registered")
+    )
+}) 
 
 export {registerUser}
